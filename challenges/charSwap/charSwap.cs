ff36175b290b0e8383b4bd80e97bc1d47ef39a65
maddyking44 <madelineking44@gmail.com>
2018-12-05 19:35:51 +1300

    First challenges completed

---
+++ challenges/charSwap/charSwap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Challenges {
+  public class multiplyThree {
+    public static void Main(string[] args) {
+        int num1, num2, num3, ans;
+        Console.WriteLine("What is the first number you want to multiply?");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("What is the next number?");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("What is the third number?");
+        num3 = Convert.ToInt32(Console.ReadLine());
+        ans = (num1 * num2 * num3);
+        Console.WriteLine("\nVoila!! " + num1 + " x " + num2 + " x " + num3 + " = " + ans);
+    }
+  }
+}
---
+++ challenges/swaps/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SoloLearn {
+  public class Program {
+    public static void Main(string[] args) {
+        int num1, num2, num3;
+        Console.WriteLine("What is the first number?");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("What is the next number?");
+        num2 = int.Parse(Console.ReadLine());
+        num3 = num1;
+        num1 = num2;
+        num2 = num3;
+        Console.WriteLine("\nVoila!! Look at your numbers now!");
+        Console.WriteLine("\nFirst Number: " + num1);
+        Console.WriteLine("\nSecond Number: " + num2);
+    }
+  }
+}
+
+// using System;
+
+// namespace myApp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
