93ca2c026082e027ecb7c4c62556ba0054c10628
maddyking44 <madelineking44@gmail.com>
2018-12-05 22:31:49 +1300

    used string.remove, word now prints with first and last letters reversed

--- challenges/charSwap/charSwap.cs
+++ challenges/charSwap/charSwap.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Text;
 
 namespace Challenges {
   public class multiplyThree {
     public static void Main(string[] args) {
-        string str;
-        List<int> myList = new List<int>();
-        Console.WriteLine("What is the string?");
-        str = (Console.ReadLine());
+        Console.WriteLine("Write something?");
+        string str = (Console.ReadLine());
         int leng = str.Length;
         char firstChar = str[0];
-        string firstLet = firstChar.ToString();
+          string firstLet = firstChar.ToString();
         char lastChar = str[leng-1];
-        string lastLet = lastChar.ToString();
+          string lastLet = lastChar.ToString();
         string newStr = str.Insert(0, lastLet);
         string secStr = newStr.Insert(leng+1, firstLet);
-        Console.WriteLine(leng);
-        Console.WriteLine(secStr);
+        string lessStr = secStr.Remove(1, 1);
+        string shortStr = lessStr.Remove(leng-1, 1);
+        Console.WriteLine(shortStr);
         }
       }
     }
