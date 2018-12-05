6301d9027e24d2ddcdac6736e0b8b928635fb402
maddyking44 <madelineking44@gmail.com>
2018-12-05 21:49:21 +1300

    letters adding to beginning and end but not removing existing letters

--- challenges/charSwap/charSwap.cs
+++ challenges/charSwap/charSwap.cs
@@ -5,15 +5,19 @@ using System.IO;
 namespace Challenges {
   public class multiplyThree {
     public static void Main(string[] args) {
-        int num1, num2, num3, ans;
-        Console.WriteLine("What is the first number you want to multiply?");
-        num1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("What is the next number?");
-        num2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("What is the third number?");
-        num3 = Convert.ToInt32(Console.ReadLine());
-        ans = (num1 * num2 * num3);
-        Console.WriteLine("\nVoila!! " + num1 + " x " + num2 + " x " + num3 + " = " + ans);
+        string str;
+        List<int> myList = new List<int>();
+        Console.WriteLine("What is the string?");
+        str = (Console.ReadLine());
+        int leng = str.Length;
+        char firstChar = str[0];
+        string firstLet = firstChar.ToString();
+        char lastChar = str[leng-1];
+        string lastLet = lastChar.ToString();
+        string newStr = str.Insert(0, lastLet);
+        string secStr = newStr.Insert(leng+1, firstLet);
+        Console.WriteLine(leng);
+        Console.WriteLine(secStr);
+        }
+      }
     }
-  }
-}
